4bedfa6b9e9a71c026b5a746fe5f892c398fb27c
Elsaadany427 <hamadabassem427@gmail.com>
2022-02-21 07:53:07 +0200

    deleted NotFounded page from home routing

--- src/app/modules/Home/home-routing.module.ts
+++ src/app/modules/Home/home-routing.module.ts
@@ -5,7 +5,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 /* Component imports */
-import { PageNotFoundComponent } from 'src/app/components/page-not-found/page-not-found.component';
 import { DeviceComponent } from './Components/device/device.component';
 import { HomeComponent } from './Components/home/home.component';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
