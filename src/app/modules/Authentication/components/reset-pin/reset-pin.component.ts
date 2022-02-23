fba37861c19aae3e988c4c8c5bdf98984040d4bb
Elsaadany427 <hamadabassem427@gmail.com>
2022-02-23 09:27:33 +0200

    Added confirm password

--- src/app/modules/Authentication/components/reset-pin/reset-pin.component.ts
+++ src/app/modules/Authentication/components/reset-pin/reset-pin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-reset-pin',
@@ -9,8 +10,11 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class ResetPinComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
   ngOnInit(): void {
 
+  }
+  ConfirmPage(): void{
+    this.router.navigate(['Login/confirm-password']);
   }
 }
