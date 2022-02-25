7abb8716ddce6f70edc2ec0296ee869fae2ffa71
Elsaadany427 <hamadabassem427@gmail.com>
2022-02-25 14:08:17 +0200

    Added loading ngContainer

--- src/app/modules/Home/Components/home/home.component.ts
+++ src/app/modules/Home/Components/home/home.component.ts
@@ -15,37 +15,12 @@ export class HomeComponent implements OnInit {
   banner_margin_bottom: boolean = false;
   SearchCity = '';
   CitiesExist !: boolean;
-  devices: city[] = [
-    {
-      address: 'Portsaid, Second area',
-      street: 'El-Salam office, Shebin El-Kom street',
-    },
-    {
-      address: 'Elshrqia, First area',
-      street: 'El-Salam office, Shebin El-Kom street',
-    },
-    {
-      address: 'Elshrqia, Second area',
-      street: 'El-Salam office, Shebin El-Kom street',
-    },
-    {
-      address: 'Ismailia, First area',
-      street: 'El-Salam office, Shebin El-Kom street',
-    },
-    {
-      address: 'Ismailia, Second area',
-      street: 'El-Salam office, Shebin El-Kom street',
-    },
-    {
-      address: 'Ismailia, Third area',
-      street: 'El-Salam office, Shebin El-Kom street',
-    },
-  ];
+
   trackByFn(index: number, device: city) {
     return index;
   }
 
-  @Output() loading: EventEmitter<boolean> = new EventEmitter<boolean>();
+  loading !: boolean;
   searchTerm = new Subject<string>();
   baseUrl = 'https://api.cdnjs.com/libraries';
   queryUrl = '?search=';
@@ -60,11 +35,11 @@ export class HomeComponent implements OnInit {
       distinctUntilChanged(),
       filter(term => term.length > 0),
     ).subscribe(searchterm => {
-      this.loading.emit(true);
+      this.loading = true;
       this._searchEntries(searchterm);
     });
     /* margin when no devices selected */
-    if (this.devices.length == 0) this.banner_margin_bottom = true
+    if (this.searchResult.length == 0) this.banner_margin_bottom = true
   }
   searchEntries(term: any): Observable<any>{
     return this.http.get(this.baseUrl + this.queryUrl + term).pipe(
@@ -76,8 +51,8 @@ export class HomeComponent implements OnInit {
   }
   _searchEntries(term: any){
     this.searchEntries(term).subscribe({
-      next: () => {this.loading.emit(false);},
-      error: () => {this.loading.emit(false);}
+      next: () => {this.loading = false;},
+      error: () => {this.loading = false;}
     })
   }
   onInput(e: any){
