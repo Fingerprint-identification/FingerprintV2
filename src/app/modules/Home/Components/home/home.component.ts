bf6ee315e6eb9cc73835fc306233f795d577c18d
Elsaadany427 <hamadabassem427@gmail.com>
2022-02-25 13:38:36 +0200

    make search with api

--- src/app/modules/Home/Components/home/home.component.ts
+++ src/app/modules/Home/Components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, filter, map, Observable, Subject } from 'rxjs';
 import { FilterPipe } from 'src/app/core/custom-pipe/filter.pipe';
 import { city } from 'src/app/core/models/city';
 
@@ -43,12 +45,44 @@ export class HomeComponent implements OnInit {
     return index;
   }
 
-  constructor(private pipe: FilterPipe) {
+  @Output() loading: EventEmitter<boolean> = new EventEmitter<boolean>();
+  searchTerm = new Subject<string>();
+  baseUrl = 'https://api.cdnjs.com/libraries';
+  queryUrl = '?search=';
+  searchResult !: Array<{name: string, latest: string}>;
 
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
+    this.searchTerm.pipe(
+      debounceTime(50),
+      distinctUntilChanged(),
+      filter(term => term.length > 0),
+    ).subscribe(searchterm => {
+      this.loading.emit(true);
+      this._searchEntries(searchterm);
+    });
     /* margin when no devices selected */
     if (this.devices.length == 0) this.banner_margin_bottom = true
   }
+  searchEntries(term: any): Observable<any>{
+    return this.http.get(this.baseUrl + this.queryUrl + term).pipe(
+      map((response: any) => {
+        this.searchResult = response.results;
+
+      })
+    )
+  }
+  _searchEntries(term: any){
+    this.searchEntries(term).subscribe({
+      next: () => {this.loading.emit(false);},
+      error: () => {this.loading.emit(false);}
+    })
+  }
+  onInput(e: any){
+    console.log(e.target.value)
+    this.searchTerm.next(e.target.value);
+  }
+
 }
