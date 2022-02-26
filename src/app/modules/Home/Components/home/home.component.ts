495bd6e825578b69c3986a8be18d88c3357db65d
Elsaadany427 <hamadabassem427@gmail.com>
2022-02-26 15:11:58 +0200

    Edit searchResult

--- src/app/modules/Home/Components/home/home.component.ts
+++ src/app/modules/Home/Components/home/home.component.ts
@@ -1,32 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, filter, map, Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, filter, Subject } from 'rxjs';
 import { FilterPipe } from 'src/app/core/custom-pipe/filter.pipe';
 import { city } from 'src/app/core/models/city';
+import { SearchService } from 'src/app/shared/services/search.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  providers:[FilterPipe]
+  providers: [FilterPipe]
 })
 export class HomeComponent implements OnInit {
   banner_margin_bottom: boolean = false;
-  SearchCity = '';
-  CitiesExist !: boolean;
-
-  trackByFn(index: number, device: city) {
-    return index;
-  }
-
   loading !: boolean;
+  errors: any = [];
   searchTerm = new Subject<string>();
-  baseUrl = 'https://api.cdnjs.com/libraries';
-  queryUrl = '?search=';
-  searchResult : Array<{name: string, latest: string}> = [];
+  searchResult: Array<{ name: string, latest: string }> = [];
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private searchServices: SearchService) {
   }
 
   ngOnInit(): void {
@@ -36,28 +28,29 @@ export class HomeComponent implements OnInit {
       filter(term => term.length > 0),
     ).subscribe(searchterm => {
       this.loading = true;
-      this._searchEntries(searchterm);
-    });
-    /* margin when no devices selected */
-    if (this.searchResult.length == 0) this.banner_margin_bottom = true
-  }
-  searchEntries(term: any): Observable<any>{
-    return this.http.get(this.baseUrl + this.queryUrl + term).pipe(
-      map((response: any) => {
-        this.searchResult = response.results;
-
-      })
-    )
-  }
-  _searchEntries(term: any){
-    this.searchEntries(term).subscribe({
-      next: () => {this.loading = false;},
-      error: () => {this.loading = false;}
+      this.searchServices._searchEntries(searchterm).subscribe(
+        {
+          next: (response: any) => {
+            this.loading = false;
+            this.searchResult = response.results;
+          }, error: (error: any) => {
+            this.errors = error;
+            this.loading = false;
+          }
+        });
+      /* margin when no devices selected */
+      if (this.searchResult.length == 0)
+        this.banner_margin_bottom = true
     })
   }
-  onInput(e: any){
-    console.log(e.target.value)
+  onInput(e: any) {
+    if(e.target.value == '')
+      this.searchResult = [];
     this.searchTerm.next(e.target.value);
+  }
+
+  trackByFn(index: number, device: city) {
+    return index;
   }
 
 }
