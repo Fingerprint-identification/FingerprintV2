cc866d9f000e972f173abc32f9386ce85f287e2e
Elsaadany427 <hamadabassem427@gmail.com>
2022-03-18 18:06:29 +0200

    Added user information *under implementation*

--- src/app/modules/Administration/pages/dashboard/dashboard.component.ts
+++ src/app/modules/Administration/pages/dashboard/dashboard.component.ts
@@ -7,14 +7,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  pages: string[] = ['scan', 'userInfo', 'familyInfo', 'done'];
+  mainUrl: string = 'Admin/signup' + '/';
+  lastUrlSegment !: string;
+  indexOfPage !: number;
 
-  pages: string[] = ['signup', 'checkFingerprint', 'searchUser'];
-  constructor(private router: Router) { }
+  newPageUrl: string = this.mainUrl + 'scan';
+
+  constructor(
+    private router: Router) {
+  }
 
   ngOnInit(): void {
   }
-  Active(page: string){
-    this.router.url.includes(`/${page}`);
-  }
 
+  GetCurrentPath() {
+    this.lastUrlSegment = this.router.url.split('?')[0].split('/').pop()!;
+    this.indexOfPage = this.pages.indexOf(this.lastUrlSegment!);
+  }
+  Back() {
+    this.GetCurrentPath();
+    if (this.indexOfPage == 0 || this.indexOfPage == -1)
+      return;
+    this.newPageUrl = this.mainUrl + this.pages[this.indexOfPage - 1];
+    this.router.navigate([this.newPageUrl]);
+  }
+  Next() {
+    this.GetCurrentPath();
+    if (this.indexOfPage == this.pages.length - 1 || this.indexOfPage == -1)
+      return;
+    this.newPageUrl = this.mainUrl + this.pages[this.indexOfPage + 1];
+    this.router.navigate([this.newPageUrl]);
+
+  }
 }
