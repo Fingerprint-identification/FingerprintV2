a0b684f2400ab85460b7ba96fadbd4e091bafa98
Elsaadany427 <hamadabassem427@gmail.com>
2022-03-31 11:54:52 +0200

    Fixed scan fingerprint code and make it clean

--- src/app/modules/Administration/components/scan-fingerprint/scan-fingerprint.component.ts
+++ src/app/modules/Administration/components/scan-fingerprint/scan-fingerprint.component.ts
@@ -1,26 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+
 import { ImgAnalysisService } from '../../shared/services/img-analysis.service';
+
 import { SignupService } from '../../shared/services/signup.service';
 
+/**
+ * @ScanFingerprintComponent here we scan fingerprint and send file to api
+ */
 @Component({
   selector: 'app-scan-fingerprint',
   templateUrl: './scan-fingerprint.component.html',
   styleUrls: ['./scan-fingerprint.component.scss']
 })
 export class ScanFingerprintComponent implements OnInit {
+  // Local storage to check if the img uploded successfully or not
   ImgUploaded: boolean = false;
 
+  /**
+   * @param signUpAuth to access methods inside signUp services
+   * @param imgAnalysisAuth to access img services to send it for analysis
+   */
   constructor(private signUpAuth: SignupService, private imgAnalysisAuth: ImgAnalysisService) { }
 
   ngOnInit(): void {
-    if (this.signUpAuth.getUserSignUpData("fingerprint") === "false") {
-      this.ImgUploaded = false;
-    } else
-      this.ImgUploaded = true;
+    // checking if image uploaded to active html image with green if true else active gray image
+    this.ImgUploaded = (this.signUpAuth.getUserSignUpData("fingerprint") === "false") ? false : true;;
   }
-  // image uploaded
-  uploadImage(event: any) {
+  /**
+   * method that detect the image uploaded and make operation in it
+   * @param event it's the file detected by function
+   * @returns void
+   */
+  uploadImage(event: any): void{
+    // select the file
     const file = event.target.files[0];
+    // check if it's empty or not
     if (event.target.files.length > 0) {
       this.ImgUploaded = true;
       var formData = new FormData();
