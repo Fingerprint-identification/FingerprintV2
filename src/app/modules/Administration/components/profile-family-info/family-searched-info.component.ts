52d5cc094f698df31cdb187801bab0fe2bee84a5
Elsaadany427 <hamadabassem427@gmail.com>
2022-04-07 09:55:11 +0200

    Added valiation form for family profile

--- src/app/modules/Administration/components/profile-family-info/family-searched-info.component.ts
+++ src/app/modules/Administration/components/profile-family-info/family-searched-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { SignupService } from '../../shared/services/signup.service';
 
 @Component({
@@ -7,10 +9,59 @@ import { SignupService } from '../../shared/services/signup.service';
   styleUrls: ['./family-searched-info.component.scss', '../../shared/admin-global-style.scss']
 })
 export class FamilySearchedInfoComponent implements OnInit {
+  // Local reference to carry Family Entered data
+  familyForm!: FormGroup;
+  // Local reference to carry Family data from cookies
   familyData !: any;
-  constructor(private signUpServices: SignupService){}
-  ngOnInit(): void {
-    this.familyData = this.signUpServices.getUserSignUpData("userInformation");
+  // Local reference to carry massegeError happened
+  massegeError !: string;
+  // Local reference to check if the form submited successfuly
+  formSubmited: boolean = false;
+  /**
+   * @param router to access some properities from router
+   * @param signUpAuth to access some signUpAuth services services from signUpAuth services
+   */
+  constructor(private router: Router, private signUpAuth: SignupService) {
+
   }
 
+  ngOnInit(): void {
+    // to check if the form submitted to display required fields that admin wasn't filled
+    // this.formSubmited = (window.localStorage.getItem("submited")) ? true: false;
+    // Get familyData stored in cookies to put it into formControl value to display to admin for access it
+    this.familyData = this.signUpAuth.getUserSignUpData("userInformation");
+     // Form validation
+    this.familyForm = new FormGroup({
+      mother_FullName: new FormControl(this.familyData.mother_FullName, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      motherNationality: new FormControl(this.familyData.motherNationality, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern('Egyption'),
+      ]),
+      motherId: new FormControl(this.familyData.motherId, [
+        Validators.required,
+        Validators.minLength(14),
+        Validators.maxLength(14),
+        Validators.pattern(/^-?(0|[1-9]\d*)?$/),
+      ]),
+      fatherFullName: new FormControl(this.familyData.fatherFullName, [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      fatherNationality: new FormControl(this.familyData.fatherNationality, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern('Egyption'),
+      ]),
+      fatherId: new FormControl(this.familyData.fatherId, [
+        Validators.required,
+        Validators.minLength(14),
+        Validators.maxLength(14),
+        Validators.pattern(/^-?(0|[1-9]\d*)?$/),
+      ]),
+    });
+  }
 }
