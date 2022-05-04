6ff713c0e2d58df0373ecf546e23808a013c5cf7
Elsaadany427 <hamadabassem427@gmail.com>
2022-05-04 19:59:42 +0200

    Added AdminData class

--- src/app/modules/manager/models/adminData.ts
+++ src/app/modules/manager/models/adminData.ts
@@ -5,21 +5,21 @@ export class AdminData {
     national_id ?: string;
     password ?: string;
     passwordConfirm  ?: string;
-    name ?: string;
-    phone  ?: string;
-    email ?: string;
-    nationality  ?: string;
-    birthday  ?: Date;
-    gender  ?: string;
-    birthplace  ?: string;
+    name ?: string = "string";
+    phone  ?: string = "01111111111"
+    email ?: string = "example@gmail.com";
+    nationality  ?: string = "Egyption";
+    birthday  ?: Date = new Date('22-03-2022T00:00:00');
+    gender  ?: string = "Male";
+    birthplace  ?: string = "cairo";
     alias ?: string;
     details ?: string;
-    city ?: string;
-    disease ?: string[];
-    fingerprint ?: any[];
-    role ?: string;
-    mother ?: string;
-    father ?: string;
+    city ?: string = "Cairo";
+    disease ?: string[] = ["e","e"];
+    fingerprint ?: any[] = [[0],[0]];
+    role ?: string = "admin";
+    mother ?: string = "99999999999999";
+    father ?: string = "88888888888888";
     constructor(values: AdminData) {
       Object.assign(this, values);
     }
